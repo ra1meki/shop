f94e88e87a6b8c7d9f6bf15ba481755455e972da
ra1meki <ashen2312rqqb@internet.ru>
2023-12-27 14:51:05 +0300

    1

--- app.js
+++ app.js
@@ -1,128 +1,110 @@
 const express = require("express");
-const fs = require("fs");
-
+const MongoClient = require("mongodb").MongoClient;
+const objectId = require("mongodb").ObjectId;
+      
 const app = express();
-const jsonParser = express.json();
-
-app.use(express.static(__dirname + "/public"));
-
-app.get("/api/products", function (req, res) {
-
-  let content = fs.readFileSync("products.json", "utf8");
-  let products = JSON.parse(content);
-  res.send(products);
-});
-
-app.get("/api/products/:id", function(req, res) {
-
-  let id = req.params.id;
-  let content = fs.readFileSync("products.json", "utf8");
-  let products = JSON.parse(content);
-  let product;
-
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id == id) {
-      product = products[i];
-      break;
-    }
-  }
-
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send("Product isn't found");
-  }
-
-});
-
-app.post("/api/products", jsonParser, function (req, res) {
-  
-  if (!req.body) res.sendStatus(400);
-
-  let productName = req.body.name;
-  let productCost = req.body.cost;
-  let product = {name: productName, cost: productCost};
-
-  let data = fs.readFileSync("products.json", "utf8");
-  let products = JSON.parse(data);
-
-  let id = Math.max(...products.map((product) => product.id));
-  
-  if (Number.isFinite(id)) {
-    product.id = id + 1;
-  } else {
-    product.id = 1;
-  }
-  
-  products.push(product);
-
-  data = JSON.stringify(products);
-  fs.writeFileSync("products.json", data);
-  res.send(product);
-});
-
-app.delete("/api/products/:id", function (req, res) {
-
-  let id = req.params.id;
-  let data = fs.readFileSync("products.json", "utf8");
-  let products = JSON.parse(data);
-  let index = -1;
-
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id == id) {
-      index = i;
-      break;
-    }
-  }
-
-  if (index > -1) {
-    let product = products.splice(index, 1)[0];
+app.use(express.static("public"));
+app.use(express.json());
     
-    for (let i = 0; i < products.length; i++) {
-      products[i].id = i + 1;
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
+   
+(async () => {
+     try {
+        await mongoClient.connect();
+        app.locals.collection = mongoClient.db("productsdb").collection("products");
+        app.listen(3000);
+        console.log("Сервер ожидает подключения...");
+    }catch(err) {
+        return console.log(err);
+    } 
+})();
+   
+app.get("/api/products", async(req, res) => {
+           
+    const collection = req.app.locals.collection;
+    try{
+        const products = await collection.find({}).toArray();
+        res.send(products);
     }
+    catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }  
+});
+app.get("/api/products/:id", async(req, res) => {
+           
+    const collection = req.app.locals.collection;
+    try{
+        const id = new objectId(req.params.id);
+        const product = await collection.findOne({_id: id});
+        if(product) res.send(product);
+        else res.sendStatus(404);
+    }
+    catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+      
+app.post("/api/products", async(req, res)=> {
+          
+    if(!req.body) return res.sendStatus(400);
+          
+    const productName = req.body.name;
+    const productCost = req.body.cost;
+    const product = {name: productName, cost: productCost};
+          
+    const collection = req.app.locals.collection;
+       
+    try{
+        await collection.insertOne(product);
+        res.send(product);
+    }
+    catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+       
+app.delete("/api/products/:id", async(req, res)=>{
+           
+    const collection = req.app.locals.collection;
+    try{
+        const id = new objectId(req.params.id);
+        const product = await collection.findOneAndDelete({_id: id});
+        if(product) res.send(product);
+        else res.sendStatus(404);
+    }
+    catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+      
+app.put("/api/products", async(req, res)=>{
+           
+    if(!req.body) return res.sendStatus(400);
+    const productName = req.body.name;
+    const productCost = req.body.cost;
+          
+    const collection = req.app.locals.collection;
+    try{
+        const id = new objectId(req.body.id);
+        const product = await collection.findOneAndUpdate({_id: id}, { $set: {cost: productCost, name: productName}},
+         {returnDocument: "after" });
+        if(product) res.send(product);
+        else res.sendStatus(404);
+    }
+    catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
     
-    let data = JSON.stringify(products);
-    fs.writeFileSync("products.json", data);
-    res.send(product);
-  } 
-  else {
-    res.status(404).send("Product isn't found by ID");
-  }
-});
-
-app.put("/api/products", jsonParser, function (req, res) {
-
-  if (!req.body) res.status(400).send("Failed to change");
-  
-  let productId = req.body.id;
-  let productName = req.body.name;
-  let productCost = req.body.cost;
-
-  let data = fs.readFileSync("products.json", "utf8");
-  let products = JSON.parse(data);
-  let product;
-
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id == productId) {
-      product = products[i];
-      break;
-    }
-  }
-
-  if (product) {
-    product.cost = productCost;
-    product.name = productName;
-    let data = JSON.stringify(products);
-    fs.writeFileSync("products.json", data);
-    res.send(product);
-  } 
-  else {
-    res.status(404).send(product);
-  }
-});
-
-app.listen(3000, function() {
-  console.log("Server started");
+process.on("SIGINT", async() => {
+       
+    await mongoClient.close();
+    console.log("Приложение завершило работу");
+    process.exit();
 });
 
